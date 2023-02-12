1942b46f3def11a5e762e07580206ce8bad6c99e
Джемми <125199050+Briariya@users.noreply.github.com>
2023-02-12 18:38:20 +0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+import time
+import datetime
+
+# Функция для отправки сообщения
+def send_message(message):
+    # Код для отправки сообщения
+    pass
+
+# Функция для проверки времени
+def check_time():
+    # Получаем текущее время
+    current_time = datetime.datetime.now()
+    # Проверяем, соответствует ли оно указанному времени
+    if current_time == datetime.datetime(2023, 2, 11, 12, 28, 0):
+        # Отправляем сообщение
+        send_message("Время пришло!")
+
+# Запускаем цикл проверки времени
+while True:
+    check_time()
+    # Задержка в 1 секунду
+    time.sleep(1)
