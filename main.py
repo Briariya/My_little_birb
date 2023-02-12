616698309dcb4594e32a0e3205e6994ccdbfc100
Den4ik-ru <83586300+Den4ik-ru@users.noreply.github.com>
2023-02-12 19:48:38 +0400

    Покорми попугая каждые 10 секунд

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-#version 0.0.2
+#version 0.0.3
 import time
 import datetime
+import telebot
+
+token = '5734661771:AAExKRzJuvg8dKRWm_RH_CV4JVWSMK0YcNc' # @popugaii_1_bot
+bot = telebot.TeleBot(token)
+@bot.message_handler(commands=['start'])
+def command_send(message):
+    text_ = "Покорми попугая"
+    bot.send_message(message.chat.id, text_)
+    time.sleep(10)
+    command_send(message)
 
 # Функция для отправки сообщения
 def send_message(message):
-    # Код для отправки сообщения
+    command_send(message)
     pass
 
 # Функция для проверки времени
@@ -16,8 +26,10 @@ def check_time():
         # Отправляем сообщение
         send_message("Время пришло!")
 
+bot.infinity_polling(skip_pending=True)
 # Запускаем цикл проверки времени
 while True:
     check_time()
     # Задержка в 1 секунду
-    time.sleep(1)
+    time.sleep(10)
+
